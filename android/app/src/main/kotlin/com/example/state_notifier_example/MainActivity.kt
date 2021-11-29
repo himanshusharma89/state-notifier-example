d12bfc6f1d4724cf6bc972eba52f8772b301f263
Himanshu Sharma <ranubhardwaj89@gmail.com>
2021-11-29 20:13:29 +0530

    Project Init

---
+++ android/app/src/main/kotlin/com/example/state_notifier_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.state_notifier_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Book Entry App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme:
+              GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              titleTextStyle:
+                  GoogleFonts.nunito(color: Colors.black, fontSize: 22))),
+      home: const MyHomePage(title: 'Flutter Book Entry App'),
+    );
+  }
+}
